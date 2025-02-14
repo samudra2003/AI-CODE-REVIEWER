f41ae9b152923d60abb95a629c3d703c29dcfd42
samudrababai <147082816+samudrababai@users.noreply.github.com>
2025-02-14 17:45:30 +0530

    completed project UI using react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <>
+    <main>
+      <div className="left">
+        <div className="code"></div>
+        <div className="review">Review</div>
+      </div>
+      <div className="right"></div>
+    </main>
+     
+    </>
+  )
+}
+
+export default App
